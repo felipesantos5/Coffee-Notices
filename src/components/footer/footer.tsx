b01d4dff34e394e34f8499724b70fcc6c271aa93
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-07-23 18:11:22 -0300

    feat: foote component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,3 +1,9 @@
 export const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer className="h-10  text-center pt-8 bg-slate-100 dark:bg-darkSlate">
+      <a className="text-black dark:text-white px-2" href="#">
+        Copyright © 2023. All rights are reserved
+      </a>
+    </footer>
+  );
 };
