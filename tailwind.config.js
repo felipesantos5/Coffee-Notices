c6ff9aa6979bbd491e03e678da381b6857f76789
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-07-23 18:10:34 -0300

    improvement: setting variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "1000px" },
+      md: { max: "767px" },
+      xmd: { max: "600px" },
+      sm: { max: "670px" },
+      xsm: { max: "500px" },
+    },
+    extend: {
+      spacing: {
+        650: "650px",
+        500: "500px",
+        80: "80vh",
+        10: "10vh",
+      },
+      colors: {
+        white: "#F5F5F5",
+        darkSlate: "#131b24",
+      },
+    },
   },
   plugins: [],
 };
