9c32737e324f49658948ce386a8196381145fc0d
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-07-24 00:16:45 -0300

    improvement: changing styles

--- src/components/darkModeButton/darkModeButton.tsx
+++ src/components/darkModeButton/darkModeButton.tsx
@@ -1,15 +1,11 @@
 import { useState, useEffect } from "react";
 
 export const DarkModeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  useEffect(() => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(() => {
     // Verifica se há um valor salvo no localStorage
     const savedMode = localStorage.getItem("darkMode");
-    if (savedMode === "true") {
-      setIsDarkMode(true);
-    }
-  }, []);
+    return savedMode === "true";
+  });
 
   const toggleDarkMode = () => {
     setIsDarkMode((prevMode) => !prevMode);
@@ -25,8 +21,8 @@ export const DarkModeToggle = () => {
   }, [isDarkMode]);
 
   return (
-    <button className={`xsm:hidden relative w-16 h-9 rounded-full transition-transform ${isDarkMode ? "bg-slate-100" : "bg-darkSlate"}`} onClick={toggleDarkMode}>
-      <div className={`absolute top-1 left-1 w-7 h-7 rounded-full transition-transform ${isDarkMode ? "bg-darkSlate" : "bg-slate-100"} ${isDarkMode ? "translate-x-full" : "translate-x-0.5"}`}></div>
+    <button className={`xsm:hidden relative w-16 h-9 rounded-full ${isDarkMode ? "bg-slate-100" : "bg-darkSlate"}`} onClick={toggleDarkMode}>
+      <div className={`absolute top-1 left-1 w-7 h-7 rounded-full transition-transform ${isDarkMode ? "bg-darkSlate translate-x-full" : "bg-slate-100 translate-x-0.5"}`}></div>
     </button>
   );
 };
