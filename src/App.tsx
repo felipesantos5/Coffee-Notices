76a728a7a66d6999f7b917c08d735e58bc493bb9
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-08-05 22:54:26 -0300

    feat: add ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import ErrorBoundary from "./utils/ErrorBoundary";
 import "./index.css";
 
 function App() {
   return (
-    <>
+    <ErrorBoundary>
       <Outlet />
-    </>
+    </ErrorBoundary>
   );
 }
 
