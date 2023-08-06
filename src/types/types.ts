c838b7f8432946bebde9e484dfb7b6f9413aafc7
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-08-05 22:54:48 -0300

    feat: add news types

---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+export interface Article {
+  title: string;
+  image: string;
+  description: string;
+  content: string;
+  publishedAt: string;
+  url: string;
+  source: {
+    name: string;
+  };
+}
