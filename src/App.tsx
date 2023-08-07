88b3a014e11fd05630255fb2307e80dc244874f9
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-08-07 18:29:56 -0300

    revert: changing to react router error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom";
-import ErrorBoundary from "./utils/ErrorBoundary";
 import "./index.css";
 
 function App() {
   return (
-    <ErrorBoundary>
+    <>
       <Outlet />
-    </ErrorBoundary>
+    </>
   );
 }
 
