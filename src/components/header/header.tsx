407011bfaccfe76207da5efcfa61c558bd0bfaef
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-10-03 17:28:38 -0300

    fix: add Link router dom in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { DarkModeToggle } from "../darkModeButton/darkModeButton";
 
 export const Header = () => {
@@ -5,7 +6,9 @@ export const Header = () => {
     <header className="h-10 p-6 bg-slate-100 dark:bg-darkSlate md:pb-9">
       <div className="flex items-center justify-between px-8 max-w-5xl m-auto md:px-0">
         <div className="flex items-center gap-1">
-          <h1 className="font-bold text-5xl text-black dark:text-slate-100">Web Notices</h1>
+          <Link to={"/"}>
+            <h1 className="font-bold text-5xl text-black dark:text-slate-100">Web Notices</h1>
+          </Link>
         </div>
         <DarkModeToggle />
       </div>
